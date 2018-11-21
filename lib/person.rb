5a2426516d62059d8c591981bf6b95b5aab6db5d
Emma Gouti <goutiemma@gmail.com>
2018-11-21 21:28:23 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,68 @@
-# your code goes here
+require 'pry'
+
+class Person
+  attr_accessor :bank_account
+  attr_reader :happiness
+  attr_reader :hygiene
+  attr_reader :name
+  
+  def initialize(name)
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+  end
+  
+  
+  def happiness=(value)
+    if value > 10 
+      @happiness = 10
+    elsif value < 0 
+      @happiness = 0
+    else
+      @happiness = value
+  end
+end
+
+def hygiene=(value)
+  if value > 10 
+      @hygiene = 10
+    elsif value < 0 
+      @hygiene = 0
+    else
+      @hygiene = value
+    end
+end
+  
+  def happy?
+    if self.happiness > 7
+      return TRUE
+    else
+      return FALSE
+    end
+end
+
+def clean?
+  if self.hygiene > 7
+    return TRUE
+  else
+    return FALSE
+  end
+end
+
+def get_paid(salary)
+  @bank_account += salary
+  return "all about the benjamins"
+end
+
+  def take_bath
+    hygiene=(4)
+    self.hygiene += @hygiene
+  #   @hygiene += 4
+  # if self.hygiene > 10
+  #   @hygiene = 10
+  # end
+    return "♪ Rub-a-dub just relaxing in the tub ♫"
+  end
+  
+end
