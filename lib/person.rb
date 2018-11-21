6bd2893245fa70bd765a5ef671d03f70b68a2530
Emma Gouti <goutiemma@gmail.com>
2018-11-21 21:40:06 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -76,7 +76,7 @@ end
   def call_friend(friend)
     self.happiness += 3
     friend.happiness += 3
-    return "Hi Penelope! It's Felix. How are you?"
+    return "Hi Felix! It's Penelope. How are you?"
 end
 
 end
